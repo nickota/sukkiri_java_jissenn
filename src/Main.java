33219c0c652ab2e8d880e7dfe40c9a8066c0b346
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-27 14:11:15 +0000

    p.62

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,12 @@ import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
-        Instant i1 = new Instant.now();
+        //LocalDateTimeの生成方法
+        LocalDateTime l1 = new LocalDateTime.now();
+        LocalDateTime l2 = new LocalDateTime.of(2014, 1, 1, 9, 5, 0, 0);
         
-        //Instantとlong値との相互変換
-        Instant i2 = new Instant.ofEpochMilli(31920291332L);
-        long l = i2.toEpochMilli();
-        
-        //ZonedDateTimeの生成
-        ZonedDateTime z1 = new ZonedDateTime.now();
-        ZonedDateTime z2 = ZonedDateTime.of(2014, 1,2,3,4,5,6,ZoneId.of("Asia/Tokyo"));
-        
-        //InstantとZonedDateTimeの相互変換
-        Instant i3 = z2.toInstant();
-        ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-        
-        //ZonedDateTimeの利用
-        System.out.println("東京:" + z2.getYear() + z2.getMonthValue() + z2.getDayOfMonth());
-        System.out.println("ロンドン:" + z3.getYear() + z3.getMonthValue() + z3.getDayOfMonth());
-        
-        if(z2.isEqual(z3)){
-            System.out.println("同じ瞬間");
-        }
+        //LocalDateTimeとZoneDateTimeの相互変換
+        ZoneDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+        LocalDateTime l3 = z1.toLocalDateTime();
     }
 }
