5c0c4f4959b46cc00a2b8fd0c1cb67caa38e589f
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-29 18:48:55 +0000

    p.70

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.time.*
+import java.time.*;
 import java.time.format.*;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate ld1 = LocalDate.of(2012,1,1);
-        LocalDate ld2 = LocalDate.of(2012,1,4);
-        
-        //三日間を表すPeriodを２通りの方法で実装
-        Period p1 = Period.ofDays(ld1, ld2);
-        Period p2 = Period.between(ld1, ld2);
-        
-        //ld2の三日後
-        LocalDate ld3 = ld2.plus(p2)
+       LocalDate now  = new LocalDate.now();
+       LocalDate future = now.plusDays(100);
+       DateTimeFormatter f = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+       System.out.println(future.format(f));
     }
 }
