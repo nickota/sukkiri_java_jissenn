5fe17c8bcb97a6855c8d4298e62488973829831c
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-01 14:52:15 +0000

    p.86

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-       ArrayList<Integer> points = new ArrayList<Integer>();
-       points.add(10);
-       points.add(20);
-       
-       for(int i: points){
-           System.out.println(i);
-       }
+
+public class Main{
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("あ");
+        names.add("い");
+        Iterator<String> it = names.iterator();
+        
+        while (it.hasNext()){
+            String e = it.next();
+            System.out.println(e);
+        }
     }
 }
