214356d0ae24290dace8fdbde4cb89fd2226383a
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-03 11:58:26 +0000

    p.100

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        Set<String> colors = new HashSet<String>();
-        colors.add("red");
-        colors.add("blue");
-        colors.add("yellow");
+        Map<String, Integer> prefs = new HashMap<String, Integer>();
+        prefs.put("京都府",255);
+        prefs.put("東京都",1261);
         
-        for(String s : colors){
-            System.out.println(s + "→");
-        }
+        int tokyo = prefs.get("京都府");
+        System.out.println(tokyo);
         
+        prefs.remove("東京都");
+        prefs.put("熊本県", 200);
+        System.out.println(prefs.get("熊本県"));
     }
 }
