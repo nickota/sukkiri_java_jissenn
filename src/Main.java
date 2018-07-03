2bbffeec587cb8a72b751b116375e87ed6923b33
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-03 12:36:37 +0000

    first

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,4 @@
 public class Main{
     public static void main(String[] args){
-        Hero hero1 = new Hero("斎藤");
-        Hero hero2 = new Hero("鈴木");
-        
-        Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
-        heroes.put(hero1, 3);
-        heroes.put(hero2, 7);
-        
-        for(Hero key : heroes.keySet()){
-            int value = heroes.get(key);
-            System.out.println(key.getName() + ":" + value);
-            
-        }
     }
 }
