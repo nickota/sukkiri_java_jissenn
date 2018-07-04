17d9525d423e4519cf7dc1324ad047132600ce35
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-04 13:18:08 +0000

    p.114

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-public class Main{
-    public static void main(String[] args){
+public class Hero{
+    private String name;
+    private int hp, mp;
+    
+    public String toString(){
+        return "勇者(名前:" + this.name + hp + mp + ")";
     }
 }
