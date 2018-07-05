d11b216bdd1c0085fc50ab677981404875dc0860
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-05 14:32:12 +0000

    p.156

--- src/Book.java
+++ src/Book.java
@@ -1,44 +1,11 @@
-import java.util.*;
+public class Account{
+    private String accountNo;
+    private int balance;
+    
+    private AccountType acountType;
+    public Account(String aNo, AccountType aType);
+}
 
-public class Book implements Comparable<Book>, Cloneable{
-    private String title;
-    private Date publishDate;
-    private String comment;
-    
-    public int hashCode(){
-        int r = 1;
-        r = 31 * r + publishDate.hashCode();
-        r = 31* r + title.hashCode();
-        return r;
-    }
-    public boolean equals(Object o){
-        if(this == o){
-            return true;
-        }
-        if(o==null){
-            return false;
-        }
-        if(!(o instanceof Book)){
-            return false;
-        }
-        Book b = (Book) o;
-        if(!publishDate.equals(b.publishDate)){
-            return false;
-        }
-        if(!title.equals(b.title)){
-            return false;
-        }
-        return true;
-    }
-    public int compareTo(Book o){
-        return this.publishDate.compareTo(o.publishDate);
-    }
-    
-    public Book clone(){
-        Book b = new Book();
-        b.title = this.title;
-        b.comment = this.comment;
-        b.publishDate = (Date) this.publishDate.clone();
-        return b;
-    }
+enum AccountType{
+    FUTSU, TOUZA, TEIKI;
 }
