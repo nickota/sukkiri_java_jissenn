9200cac0afcdc478927abfa427cb35054be1fa20
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-08 09:00:05 +0000

    p.185

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,1 @@
-import java.util.function.*;
-
-public class Main {
-    public static int sub(int a, int b){
-        return a - b;
-    }
-    
-    public static void main(String[] args){
-        //subメソッドの処理ロジックを変数funcに代入する
-        IntBinaryOperator func = Main::sub;
-        int a = func.applyAsInt(5, 3);
-        System.out.println(a );
-    }
-}
+(Hero h) -> {return h.getHp();}
