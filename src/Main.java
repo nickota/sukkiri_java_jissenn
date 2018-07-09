b3d0f1b5fe8f6c9075fb4dae615eaa77945a2255
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-09 12:40:05 +0000

    p.185 ラムだ式意味不明

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,16 @@
+//勇者インスタンスを受け取り、そのHPを返す
 (Hero h) -> {return h.getHp();}
+//何も受け取らず、現在日時を返す
+() -> {return new java.util.Date();}
+//long配列を受け取り、そのコピーを作り、内容を並び替えて返す
+(long[] array) -> {
+    long[] array2 = java.util.Arrays.copyOf(array, array.length);
+    java.util.Arrays.sort(array2);
+    return array2;
+}
+
+//関数オブジェクトを受け取り、2回呼び出した合計を返す
+(IntBinaryOperator func, int a, int b) -> {
+    int result = func.applyAsInt(a, b) + func.applyAsInt(a, b);
+    return result;
+}
