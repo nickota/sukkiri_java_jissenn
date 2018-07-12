7a2e47d01170d244d09e2fff28c09427c0373849
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-12 12:12:47 +0000

    p.254 iの中に何が入るかわかってない

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-import org.apache.commons.logging.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args){
-        Log logger = LogFactory.getLog(Main.class);
-        if(args.length != 2){
-            logger.error("起動引数の数が異常：" + args.length);
+public class Main{
+    public static void main(String[] args) throws IOException{
+        FileReader fw = new FileReader("rpgsave.dat");
+        System.out.println("すべてのデータを読んで表示します");
+        int i = fw.read();//1文字読み込んでるけど、なに入ってるかわからない
+        //これ以上読み込める値がないと-1になる
+        while(i != -1){
+            char c = (char) i;
+            System.out.println(c);
+            i = fw.read();
         }
+        System.out.println("ファイルの末尾に到達しました");
+        fw.close();
     }
 }
