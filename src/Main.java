5a020e32acf012f42810810b9ad7d40f2431f698
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-14 08:50:08 +0000

    p.348

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,10 @@
-import java.sql.*;
-    //STEP 0 : 事前準備(JARの配置を含む)
-    try {
-        //これとgetConnectionでDBに接続できるようになる
-        Class.forName("org.h2.Driver");
-    }catch(ClassNotFoundException e){
-        e.printStackTrace();
-    }
-    
-    Connection con = null;
-    try{
-        //STEP 1 : データベースの接続
-        con = DriverManager.getConnection("jdbc:h2:~/rpgdb"); //JDBC URLを指定している
-        //STEP 2 : SQLの送信処理
-        /*
-        メインのDB操作
-        */
-    }catch(SQLException e){
-        e.printStackTrace();
-    }finally{
-        //STEP 3 : データベース接続の切断
-        if(con != null){
-            try{
-                con.close();
-            }catch(SQLException e){
-                e.printStackTrace();
-            }
-        }
-    }
+PreparedStatement pstmt = conn.prepareStatement("DELETE FROM MONSTERS WHERE HP~~");
+
+pstmt.setInt(1, 10);
+pstmt.setString(2, "ゾンビ");
+int r = pstmt.executeUpdate();
+
+if(r != 0){
+    System.out.println(~~);
+}
+pstmt.close();
