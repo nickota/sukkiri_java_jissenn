aaa0bff42ec9ff042f0484fe5dc87c504d7b4f38
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-16 13:38:01 +0000

    p.353

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-Preparedstatement pstmt = con.prepareStatement("DELETE FROM ~");
-pstmt.setString(1, "すずめ");
-pstmt.executeUpdate();
-pstmt.setString(1,"わし");
-pstmt.executeUpdate();
+//送信するSQLの雛形を準備
+PreparedStatement pstmt = con.preparedStatement("SELECT * FROM MONSTERS WHERE HP >= ?");
+
+//雛形に値を流し込みSQL文を組み立てて送信する
+pstmt.setInt(1, 10);   //1番目の？に10を流しこむ
+ResultSet rs = pstmt.executeQuery();
+
+//結果表の取得
+rs.close();
 pstmt.close();
