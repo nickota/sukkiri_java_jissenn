504d79877f88828aac4e235f12e9431d067a1941
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-17 12:24:53 +0000

    p.356

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-//送信するSQLの雛形を準備
-PreparedStatement pstmt = con.preparedStatement("SELECT * FROM MONSTERS WHERE HP >= ?");
-
-//雛形に値を流し込みSQL文を組み立てて送信する
-pstmt.setInt(1, 10);   //1番目の？に10を流しこむ
-ResultSet rs = pstmt.executeQuery();
-
-//結果表の取得
-rs.close();
-pstmt.close();
+if(rs.next()){
+    System.out.println("HPは" + rs.getInt("HP"));
+}else{
+    System.out.println("DBに存在しません");
+}
