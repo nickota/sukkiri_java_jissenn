e69dc5fd31cb18074ea6a6debd31ea056f983d22
Kota Takeda <xxk0ta0605@gmail.com>
2018-07-18 11:24:08 +0000

    p.365

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,22 @@
-import java.sql.*;
-
-//STEP 0 : 事前準備
-try{
-    Class.forName("org.h2.Driver");
-}catch(ClassNotFoundException e){
-    e.printStackTrace();
+public class Item {
+    private String name;
+    private int price;
+    private int weight;
+    
+    public String getName(){
+        return this.name;
+    }
+    public int getPrice(){
+        return this.price;
+    }
+    public int getWeight(){
+        return this.weight;
+    }
+    
+    public setName(String name){
+        this.name = name;
+    }
+    public setPrice(){
+        ・・・
+    }
 }
-
- Connection conn = null;
- try{
-     //STEP 1 : データベースの接続
-     conn = DriverManager.getConnection("jdbc:h2:~/rpgdb");
-     conn.setAutoCommit(false);
-     
-     //STEP 2 : SQL送信処理
-    /*  メインのDB処理  */
-    conn.commit();
- }catch(SQLException e){
-     try{
-         conn.rollback();
-     }catch(SQLException e2) {
-         e2.printStackTrace();
-     }finally{
-         //STEP3 : 切断
-         if(conn != null){
-             try{
-                 conn.close();
-             }catch(SQLException e3){
-                 e3.printStackTrace();
-             }
-         }
-     }
- }
